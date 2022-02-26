fdcb41d465a7ad1911f4d704d987cc1233f4a00d
ilushaShadrin <97177493+ilushaShadrin@users.noreply.github.com>
2022-02-26 17:31:01 +0300

    fix_settings_33

--- bot/urls.py
+++ bot/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SearchResultsView,HomePageView
+from .views import SearchResultsView, HomePageView
 urlpatterns = [
     path('bot/', SearchResultsView.as_view(), name='search_results'),
     path('', HomePageView.as_view(), name='home'),
