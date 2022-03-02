2aae148c9de54e7ce2634dd75a9c32e37c340e37
ilushaShadrin <97177493+ilushaShadrin@users.noreply.github.com>
2022-03-02 07:19:53 +0300

    fix_settings_40

--- bot/urls.py
+++ bot/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SearchResultsView, HomePageView
+from .views import HomePageView, get_queryset
+from bot import views
 urlpatterns = [
-    path('bot/', SearchResultsView.as_view(), name='search_results'),
+    path('bot/', get_queryset, name='search_results'),
     path('', HomePageView.as_view(), name='home'),
 ]
--- bot/views.py
+++ bot/views.py
@@ -1,18 +1,46 @@
 from .models import Bb
 from django.views.generic import ListView, TemplateView
+from django.http import HttpResponse
 from django.db.models import Q
+from django.shortcuts import render
+import requests
+def search_form(request):
+    return render('home.html')
+
+# def search(request):
+#     if 'q' in request.GET:
+#         message = 'You searched for: %r' % request.GET['q']
+#     else:
+#         message = 'You submitted an empty form.'
+#     return HttpResponse(message)
+
+# def search(request):
+#     if request.method == 'GET':
+#         book_name = request.GET.get('q')
+#         try:
+#             status = Bb.objects.filter(inn=book_name)
+#             return render(request, "search_results.html", {"tro": status})
+#         except:
+#             return render(request, "search_results.html", {'tro': status})
+#     else:
+#         return render(request, "search_results.html", {})
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-class SearchResultsView(ListView):
-    model = Bb
-    template_name = 'search_results.html'
+# class SearchResultsView(ListView):
+#     model = Bb
+#     template_name = 'search_results.html'
 
-    def get_queryset(self):  # новый
-        query = self.request.GET.get('q')
-        object_list = Bb.objects.filter(
-            Q(inn__icontains=query) | Q(name__icontains=query) | Q(url_doc__icontains=query))
-        return object_list
+def get_queryset(request):  # новый
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q']
+        books = Bb.objects.filter(inn__icontains=q)
+        context = {'books': books, 'query': q}
+        return render(request, 'search_results.html', context)
+    else:
+        return HttpResponse('Пустая форма.')
+
+
 
