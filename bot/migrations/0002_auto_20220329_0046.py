53899f3722d7126a47d9b9fb55c00cb86c36f078
ilushaShadrin <97177493+ilushaShadrin@users.noreply.github.com>
2022-04-01 16:56:42 +0300

    the_end_1

--- bot/migrations/0002_auto_20220329_0046.py
+++ bot/migrations/0002_auto_20220329_0046.py
@@ -5,19 +5,19 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tulza.settings")
 django.setup()
 import pickle
 from django.db import migrations
-with open('list_actual_agency_sort', 'rb') as fp:
-    _const_d = pickle.load(fp)
-from bot.models import Bb
-
-
-def combine_names(apps, schema_editor):
-    for dct in _const_d:
-        for tro in dct:
-            lo = int(dct[tro][0])
-            print(lo)
-            obj = Bb(name=tro, id_ogr=lo, inn=dct[tro][1])
-            print(obj)
-            obj.save()
+# with open('list_actual_agency_sort', 'rb') as fp:
+#     _const_d = pickle.load(fp)
+# from bot.models import Bb
+#
+#
+# def combine_names(apps, schema_editor):
+#     for dct in _const_d:
+#         for tro in dct:
+#             lo = int(dct[tro][0])
+#             print(lo)
+#             obj = Bb(name=tro, id_ogr=lo, inn=dct[tro][1])
+#             print(obj)
+#             obj.save()
 
 
 class Migration(migrations.Migration):
@@ -27,5 +27,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(combine_names),
+        # migrations.RunPython(combine_names),
     ]
